f844c2278b8e13dbc584b00b2686bce4abfbb4af
Melvin Giovanni Domingo <domingotaiga@gmail.com>
2024-09-08 22:07:48 +0800

    b

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,7 +8,7 @@ import project6 from "../assets/projects/todo.png";
 export const HERO_CONTENT = `I am a passionate freelance full stack web developer specializing in creating interactive and reliable web applications. With a strong foundation in HTML, CSS, JavaScript, Bootstrap, React JS, Tailwind CSS, PHP, and MySQL. I enhance user experiences through proficient front-end and back-end development. My strength lies in my ability to translate conceptual ideas into efficient and sustainable web solutions. `;
 
 export const ABOUT_TEXT = `
-I am Melvin Giovanni B. Domingo, a 20-year-old second-year college student currently pursuing a Bachelor’s degree in Information Technology. My journey into programming began during a senior high school capstone project in 2022 when I discovered my interest in web development. This interest captivated me and made me curious, leading me to spend countless hours learning, developing, and experimenting with various aspects of web development.
+I am Melvin Giovanni B. Domingo, a 20-year-old third-year college student currently pursuing a Bachelor’s degree in Information Technology. My journey into programming began during a senior high school capstone project in 2022 when I discovered my interest in web development. This interest captivated me and made me curious, leading me to spend countless hours learning, developing, and experimenting with various aspects of web development.
 
 Fast forward to today, I find myself fully immersed in the technology space, thoroughly enjoying it. I actively seek to improve my knowledge and skills across different technologies and try to gain as much experience as possible in this field. 
  
