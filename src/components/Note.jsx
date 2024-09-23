45cf54fc8192c0bb691ee2410cfec20a368f1132
Melvin Giovanni Domingo <domingotaiga@gmail.com>
2024-09-23 23:23:11 +0800

    edited note

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,8 +2,7 @@ const Note = () => {
   return (
     <div>
       <p>
-        Please note that the current functionalities of this website have been
-        modified by the current admin.
+      Please note that the current functionalities of this website are now managed by the current admin, and are beyond my control.
       </p>
     </div>
   );
